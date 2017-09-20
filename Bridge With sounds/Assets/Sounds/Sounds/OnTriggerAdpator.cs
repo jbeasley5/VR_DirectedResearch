a2a6f03b8dbfba95617690f51a2314e3fa155fdf
jbeasley5 <32136234+jbeasley5@users.noreply.github.com>
2017-09-20 14:02:25 -0500

    Bridge_With_Sounds

---
+++ Bridge With sounds/Assets/Sounds/Sounds/OnTriggerAdpator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerAdpator : MonoBehaviour {
+
+	public UnityEvent whenTriggerEnter;
+	public UnityEvent whenTriggerExit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+		
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("ENTERED TRIGGER AREA");
+		whenTriggerEnter.Invoke ();
+	}
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log ("EXITED TRIGGER AREA");
+		whenTriggerExit.Invoke ();
+	}
+}
